4ed3e5c7e4ac180aed015183939f0b649c58dde7
Mauriele <mauriele92@hotmail.com>
2022-05-05 20:56:35 -0300

    first commit

---
+++ cinema.js
@@ -0,0 +1,86 @@
+// const textoBoasVindas = "Sejam bem vindos ao cinema da DH!";
+
+// module.exports = textoBoasVindas;
+const filmes = require('./database/catalogo.json')
+
+
+function adicionarFilme(filme) { // ouvinte adicionar filme
+    filmes.push(filme)
+}
+
+
+function buscarFilme(codigo) {
+    return filmes.find((filme) => {
+        return filme.codigo === codigo // true || false
+    })
+}
+const listarTodosOsFilmes = () => {
+    for(let i = 0; i < filmes.length; i++) {
+        const filme = filmes[i];
+
+        console.log(`
+        Filme: ${filme.titulo}
+        Lançou no ano: ${filme.anoLancamento}
+        ${filme.emCartaz ? 'Filme esta em cartaz' : 'Filme nao esta em cartaz'}
+     `)
+
+    }
+}
+listarTodosOsFilmes();
+
+const listarFilmesEmCartaz = () => {
+    for(let i = 0; i < filmes.length; i++) {
+        const filme = filmes[i];
+
+        if(filme.emCartaz) {
+            console.log(`
+            Filme: ${filme.titulo}
+            Lançou no ano: ${filme.anoLancamento}
+            ${filme.emCartaz ? 'Filme esta em cartaz' : 'Filme nao esta em cartaz'}
+            `)
+        }
+    }
+}
+
+const alterarStatusEmCartaz = (codigo, callback) => {
+    const filmeEncontrado = callback(codigo);
+        
+    
+    filmeEncontrado.emCartaz = !filmeEncontrado.emCartaz;
+    console.log(filmeEncontrado);
+}
+
+adicionarFilme({
+    codigo: 3,
+    titulo: 'Homem Aranha 3',
+    duracao: 3.5,
+    atores: ['fulano 1', 'fulano 2', 'fulano 3'],
+    anoLancamento: 2021,
+    emCartaz: false
+});
+
+// const filmeEncontrado = buscarFilme(2);
+
+// filmeEncontrado.emCartaz = !filmeEncontrado.emCartaz
+// console.log('Filme encontrado:', filmeEncontrado);
+alterarStatusEmCartaz(2, buscarFilme);
+
+
+
+const  listarFilmesDeLongaDuracao = () => {
+     return filmes.filter((filme) => {
+     return filme.duracao >= 2;
+})  
+}
+const filmesFiltradosDuracaoLonga = listarFilmesDeLongaDuracao();
+console.log(filmesFiltradosDuracaoLonga);
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,20 @@
+//console.log("Olá, Cinema!");
+
+const cinema = require("./cinema");
+
+console.log(cinema);
+
+//console.log("Estamos conhecendo o nodemon");
+
+console.log("Estamos aprendendo sobre o nodemon");
+
+console.log("Cinema da Digital House");
+console.log("CineHouse");
+
+
+
+
+
+
+
+
